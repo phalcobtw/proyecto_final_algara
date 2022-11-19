3763be83e9f901d091909b5b20c7ab0b85288689
phalcobtw <laxz_16@hotmail.com>
2022-11-19 14:15:53 -0700

    vista principal con todos sus botones

--- lib/vistas/vista_pantallainicio.dart
+++ lib/vistas/vista_pantallainicio.dart
@@ -16,7 +16,7 @@ class _VistaPantallaInicioState extends State<VistaPantallaInicio> {
   bool isButtonActive = false;
   late TextEditingController controller;
   @override
-  void initState(){
+  void initState() {
     super.initState();
     controller = TextEditingController();
     controller.addListener(() {
@@ -25,18 +25,74 @@ class _VistaPantallaInicioState extends State<VistaPantallaInicio> {
       setState(() => this.isButtonActive = isButtonActive);
     });
   }
+
   @override
-  void dispose(){
+  void dispose() {
     controller.dispose();
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(onPressed: (){}, child: Text('Ver Personajes')),
-      ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(child: Text('VER LISTA COMPLETA SIN FILTRAR',style: const TextStyle(fontWeight: FontWeight.bold),),height: 30,),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: () {}, child: Text('Ver Personajes')),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: () {}, child: Text('Ver Hechizos')),
+            ),
+            SizedBox(height: 75),
+            SizedBox(height: 30,child: Text('VER LISTA FILTRADA',style: const TextStyle(fontWeight: FontWeight.bold),),),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: (){}, child: Text('Ver Estudiantes')),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: (){}, child: Text('Ver Profesores')),
+            ),
+            SizedBox(height: 75),
+            SizedBox(height: 30,child: Text('VER ESCUELAS/CASAS',style: const TextStyle(fontWeight: FontWeight.bold),),),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: (){}, child: Text('Gryffindor')),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: (){}, child: Text('Hufflepuff')),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: (){}, child: Text('Ravenclaw')),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: 200,
+              height: 35,
+              child: ElevatedButton(onPressed: (){}, child: Text('Slytherin')),
+            ),
+            SizedBox(height: 15),
+          ],
+        ),
+      ),
     );
   }
 }
