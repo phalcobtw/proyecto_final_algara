3b26d15d4cb90e0017dc92a726cdcde48a283a8e
phalcobtw <laxz_16@hotmail.com>
2022-11-26 15:51:32 -0700

    metodo para sacar set de las varas

--- lib/repositorios/repositorio_personaje.dart
+++ lib/repositorios/repositorio_personaje.dart
@@ -1,9 +1,11 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:proyecto_final/problemas.dart';
 import 'package:proyecto_final/registro/registro_personaje.dart';
+import 'package:proyecto_final/registro/registro_wand.dart';
 
 abstract class RepositorioPersonaje{
   Either<Problemas, Set<Personaje>> obtenerPersonaje(dynamic jsonDePersonajes);
+  Either<Problemas, Set<Wand>> obtenerInfoWands(dynamic jsonDePersonajes);
 }
 
 class RepositorioPersonajePruebas extends RepositorioPersonaje{
@@ -19,5 +21,19 @@ class RepositorioPersonajePruebas extends RepositorioPersonaje{
       return Left(VersionDeJsonInvalida());
     }
   }
-
+  
+  @override
+  Either<Problemas, Set<Wand>> obtenerInfoWands(jsonDePersonajes) {
+    try {
+      Set<Wand> setWands = {};
+      for (var wands in jsonDePersonajes) {
+        if (wands["wand"]["wood"] != "") {
+          setWands.add(Wand.constructor(jsonElement: wands));
+        }
+      }
+      return Right(setWands);
+    } catch (e) {
+      return Left(VersionDeJsonInvalida());
+    }
+  }
 }
