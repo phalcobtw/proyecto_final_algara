a23f12e075fa815e05ef7317fe1518c948bbdc7c
phalcobtw <laxz_16@hotmail.com>
2022-11-26 17:44:23 -0700

    obtengo json de la api

---
+++ lib/repositorios/repositorio_json.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:proyecto_final/problemas.dart';
+import 'package:http/http.dart' as http;
+
+abstract class RepositorioJson{
+  Future<Either<Problemas, dynamic>> obtenerJSONPersonaje();
+  Future<Either<Problemas, dynamic>> obtenerJSONSpells();
+}
+
+
+class RepositorioJsonOnline extends RepositorioJson{
+  @override
+  Future<Either<Problemas, dynamic>> obtenerJSONPersonaje() async {
+    try {
+      final respuesta = await http.get(Uri.parse('https://hp-api.herokuapp.com/api/characters'));
+      String docJsonPersonajes = respuesta.body;
+      JsonDecoder decoder = const JsonDecoder();
+      final objeto = decoder.convert(docJsonPersonajes);
+      return Right(objeto);
+    } catch (e) {
+      return Left(VersionDeJsonInvalida());
+    }
+  }
+
+  @override
+  Future<Either<Problemas, dynamic>> obtenerJSONSpells() async{
+    try {
+      final respuesta = await http.get(Uri.parse('https://hp-api.herokuapp.com/api/spells'));
+      String docJsonSpells = respuesta.body;
+      JsonDecoder decoder = const JsonDecoder();
+      final objeto = decoder.convert(docJsonSpells);
+      return Right(objeto);
+    } catch (e) {
+      return Left(VersionDeJsonInvalida());
+    }
+  }
+
+}
